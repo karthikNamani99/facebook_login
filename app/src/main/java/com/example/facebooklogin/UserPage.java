29c42446434e898fbd0b46e21485915db74963f0
karthik <karthiknamani99@gmail.com>
2019-10-21 15:22:49 +0530

    hello

---
+++ app/src/main/java/com/example/facebooklogin/UserPage.java
@@ -0,0 +1,15 @@
+package com.example.facebooklogin;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userpage_main);
+    }
+}
